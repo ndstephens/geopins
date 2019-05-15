2a0c4817b52761ed728084d6e9edfdcece77f63b
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-15 15:49:43 -0400

    connect to DB

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
+
 const { ApolloServer } = require('apollo-server')
+const mongoose = require('mongoose')
 
 const typeDefs = require('./typeDefs')
 const resolvers = require('./resolvers')
@@ -7,5 +10,13 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 })
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connected...'))
+  .catch(err => console.error(err))
 
 server.listen().then(({ url }) => console.log(`Server listening at ${url}`))
