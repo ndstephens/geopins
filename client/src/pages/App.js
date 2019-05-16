291017066fc48257793c2a8f14983276f8409e9c
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-16 19:15:24 -0400

    create and style interactive Map

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import withRoot from '../withRoot'
 
 import Header from '../components/Header'
+import Map from '../components/Map'
 
 const App = () => {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <Map />
+    </>
   )
 }
 
