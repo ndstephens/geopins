39aca8ed771c2f7c0a37cea39f7d3ae085e33e44
Nathaniel Stephens <ndstephens@gmail.com>
2019-05-17 13:54:59 -0400

    building and styling blog section components

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -1,55 +1,115 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-// import TextField from "@material-ui/core/TextField";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import AddAPhotoIcon from "@material-ui/icons/AddAPhotoTwoTone";
-// import LandscapeIcon from "@material-ui/icons/LandscapeOutlined";
-// import ClearIcon from "@material-ui/icons/Clear";
-// import SaveIcon from "@material-ui/icons/SaveTwoTone";
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import AddAPhotoIcon from '@material-ui/icons/AddAPhotoTwoTone'
+import LandscapeIcon from '@material-ui/icons/LandscapeOutlined'
+import ClearIcon from '@material-ui/icons/Clear'
+import SaveIcon from '@material-ui/icons/SaveTwoTone'
 
 const CreatePin = ({ classes }) => {
-  return <div>CreatePin</div>;
-};
+  return (
+    <form className={classes.form}>
+      {/* FORM HEADER */}
+      <Typography
+        className={classes.alignCenter}
+        component="h2"
+        variant="h4"
+        color="secondary"
+      >
+        <LandscapeIcon className={classes.iconLarge} />
+        Pin Location
+      </Typography>
+
+      <div>
+        {/* PIN TITLE INPUT */}
+        <TextField name="title" label="Title" placeholder="Insert Pin title" />
+        {/* PIN PHOTO INPUT -- HIDDEN */}
+        <input
+          accept="image/*"
+          id="image"
+          type="file"
+          className={classes.input}
+        />
+        {/* PIN PHOTO INPUT -- ICON BUTTON */}
+        <label htmlFor="image">
+          <Button component="span" size="small" className={classes.button}>
+            <AddAPhotoIcon />
+          </Button>
+        </label>
+      </div>
+
+      {/* PIN TEXT CONTENT INPUT */}
+      <div className={classes.contentField}>
+        <TextField
+          name="content"
+          label="Content"
+          multiline
+          rows="6"
+          margin="normal"
+          fullWidth
+          variant="outlined"
+        />
+      </div>
+      <div>
+        <Button className={classes.button} variant="contained" color="primary">
+          <ClearIcon className={classes.leftIcon} />
+          Discard
+        </Button>
+        <Button
+          type="submit "
+          className={classes.button}
+          variant="contained"
+          color="secondary"
+        >
+          Submit
+          <SaveIcon className={classes.rightIcon} />
+        </Button>
+      </div>
+    </form>
+  )
+}
 
 const styles = theme => ({
   form: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    paddingBottom: theme.spacing.unit
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    paddingBottom: theme.spacing.unit,
   },
   contentField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: "95%"
+    width: '95%',
   },
   input: {
-    display: "none"
+    display: 'none',
   },
   alignCenter: {
-    display: "flex",
-    alignItems: "center"
+    display: 'flex',
+    alignItems: 'center',
   },
   iconLarge: {
     fontSize: 40,
-    marginRight: theme.spacing.unit
+    marginRight: theme.spacing.unit,
   },
   leftIcon: {
     fontSize: 20,
-    marginRight: theme.spacing.unit
+    marginRight: theme.spacing.unit,
   },
   rightIcon: {
     fontSize: 20,
-    marginLeft: theme.spacing.unit
+    marginLeft: theme.spacing.unit,
   },
   button: {
     marginTop: theme.spacing.unit * 2,
     marginBottom: theme.spacing.unit * 2,
-    marginRight: theme.spacing.unit,
-    marginLeft: 0
-  }
-});
+    // marginRight: theme.spacing.unit,
+    marginLeft: 0,
+  },
+})
 
-export default withStyles(styles)(CreatePin);
+export default withStyles(styles)(CreatePin)
--- client/src/components/Pin/NoContent.js
+++ client/src/components/Pin/NoContent.js
@@ -1,21 +1,36 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
-// import Explore from "@material-ui/icons/Explore";
-// import Typography from "@material-ui/core/Typography";
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
 
-const NoContent = ({ classes }) => <div>NoContent</div>;
+import ExploreIcon from '@material-ui/icons/Explore'
+import Typography from '@material-ui/core/Typography'
+
+const NoContent = ({ classes }) => (
+  <div className={classes.root}>
+    <ExploreIcon className={classes.icon} />
+    <Typography
+      noWrap
+      component="h2"
+      variant="h6"
+      align="center"
+      color="textPrimary"
+      gutterBottom
+    >
+      Click on map to create new Pin
+    </Typography>
+  </div>
+)
 
 const styles = theme => ({
   root: {
-    display: "flex",
-    alignItems: "center",
-    flexDirection: "column",
-    justifyContent: "center"
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
   },
   icon: {
     margin: theme.spacing.unit,
-    fontSize: "80px"
-  }
-});
+    fontSize: '80px',
+  },
+})
 
-export default withStyles(styles)(NoContent);
+export default withStyles(styles)(NoContent)
